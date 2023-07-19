e477ff2ad2947cacf5566aea207dcd17da74a3f7
luca-joe <139750387+luca-joe@users.noreply.github.com>
2023-07-19 14:12:47 +0100

    moving

---
+++ Learning_Materials/week 4 (was 6)/myVisualiser.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+def cluster_and_visualise(datafilename, K, featureNames):
+    ##your code goes here
+    pass
