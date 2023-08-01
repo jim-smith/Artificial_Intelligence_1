5be6ada8a2170d3c1aed700863e96b5588b357eb
Jim-smith <jim-smith@users.noreply.github.com>
2023-08-01 02:08:10 +0100

    refactored to create solution class and pass in name of problem module

--- Learning_Materials/week 2 (was 11)/candidateSolution.py
+++ Learning_Materials/week 2 (was 11)/candidateSolution.py
@@ -1,8 +1,13 @@
-#======================================================
+"""
+candidatesolution.py
+Implements a very simple class to hold all the variables associated with a candidate solution to a search problem.
+Should probably use some private attributes and provide getters and setters
+Author: Jim Snith 2023
+"""
 class CandidateSolution:
     def __init__(self):
-        self.variableValues = []
-        self.quality = 0
+        self.variable_values = []
+        self._quality = 0
         self.depth=0
-        self.breaks_constraints = False
+        self._breaks_constraints = False
         self.reason =""
--- Learning_Materials/week 2 (was 11)/foxchickengrain.py
+++ Learning_Materials/week 2 (was 11)/foxchickengrain.py
@@ -1,20 +1,7 @@
-
-
-import copy
-#======================================================
-class candidateSolution:
-    def __init__(self):
-        self.variableValues = []
-        self.quality = 0
-        self.depth=0
-        self.breaks_constraints = False
-        self.reason =""
-
-
-
-#======================================================        
+from candidatesolution import CandidateSolution
+       
 #python 3 lets us define the types of parameters if we want to
-def IsAtGoal(soln:candidateSolution): 
+def is_at_goal(soln:CandidateSolution): 
     if(soln.quality==1):
         return True
     else:
@@ -23,94 +10,110 @@ def IsAtGoal(soln:candidateSolution):
     
 #======================================================
 
-# define the encoding we will use for moves    
-moveNames = ["empty_0to1","Grain_0to1","Chicken_0to1","Fox_0to1","empty_1to0","Grain_1to0", "Chicken_1to0", "Fox_1to0"]    
+# define the encoding we will use for moves 
+possible_moves= [0,1,2,3,4,5,6,7]
+move_names = ["empty_0to1","Grain_0to1","Chicken_0to1","Fox_0to1","empty_1to0","Grain_1to0", "Chicken_1to0", "Fox_1to0"]    
     
 #moveNames = ["b01","G01","C01","F01","b10","G10", "C10", "F10"]    
     
     
     
 #=====================================================    
-def Evaluate(soln: candidateSolution ):
+def evaluate(soln: CandidateSolution ):
     location = [0,0,0,0]
-    global reason
-    reason = ""
     boat=3
     grain=2
     chicken=1
     fox=0
-    for move in soln.variableValues:
-        valid = True
-        if(move==0): 
+    #set initial values for solution
+    soln.quality=0 #valid but incomplete
+    soln.reason = ""
+    soln.breaks_constraints = False
+    #valid is whether boats and cargo are in right place
+    #different to constr
+    valid = True
+    for next_move in soln.variable_values:
+        if(valid and next_move==0): 
             if(location[boat]!=0):
                 valid = False
-                reason = "boat is in wrong place"
+                soln.breaks_constraints= True
+                soln.reason = "boat is in wrong place"
                 soln.quality=-1
                 break
             else:
                 location[boat]=1
-        elif(move==1):
+        elif(valid and next_move==1):
             if( location[boat]!=0 or location[grain]!=0):
                 valid = False
-                reason = "boat and/or grain is in wrong place"
+                soln.breaks_constraints= True
+                soln.reason = "boat and/or grain is in wrong place"
                 soln.quality=-1
                 break
             else:
                 location[boat] = location[grain] = 1
-        elif(move==2):
+        elif(valid and next_move==2):
             if( location[boat]!=0 or location[chicken !=0]):
                 valid = False
-                reason = "boat and/or chicken is in wrong place"
+                soln.breaks_constraints= True
+                soln.reason = "boat and/or chicken is in wrong place"
                 soln.quality=-1
                 break
             else:
                 location[boat] = location[chicken] = 1
-        elif(move==3):
+        elif(valid and next_move==3):
             if( location[boat]!=0 or location[fox]!=0):
                 valid = False
-                reason = " boat and/or fox is in wrong place"
+                soln.breaks_constraints= True
+                soln.reason = " boat and/or fox is in wrong place"
                 soln.quality=-1
                 break
             else:
                 location[boat] = location[fox] = 1                
-        elif(move==4): 
+        elif(valid and next_move==4): 
             if(location[boat]!=1):
                 valid = False
-                reason = "boat is in wrong place"
+                soln.breaks_constraints= True
+                soln.reason = "boat is in wrong place"
                 soln.quality=-1
                 break
             else:
                 location[boat]=0
-        elif(move==5):
+        elif(valid and next_move==5):
             if( location[boat]!=1 or location[grain]!=1):
                 valid = False
-                reason = "boat and/or grain is in wrong place"
+                soln.breaks_constraints= True
+                soln.reason = "boat and/or grain is in wrong place"
                 soln.quality=-1
                 break
             else:
                 location[boat] = location[grain] = 0
-        elif(move==6):
+        elif(valid and next_move==6):
             if( location[boat]!=1 or location[chicken !=1]):
                 valid = False
-                reason = " boat and/or chicken is in wrong place"
+                soln.breaks_constraints= True
+                soln.reason = " boat and/or chicken is in wrong place"
                 soln.quality=-1
                 break
             else:
                 location[boat] = location[chicken] = 0
-        elif(move==7):
+        elif(valid and next_move==7):
             if( location[boat]!=1 or location[fox]!=1):
                 valid = False
-                reason = " boat and/or fox is in wrong place"
+                soln.breaks_constraints= True
+                soln.reason = " boat and/or fox is in wrong place"
                 soln.quality=-1
                 break
             else:
                 location[boat] = location[fox] = 0                         
  
         else:
-            print('error- unknown move encountered: ' +str(move))
+            reason= 'error- unknown move encountered: ' +str(move)
+            soln.breaks_constraints= True
+            soln.reason= reason
+            soln.quality=-1
             
-        # check for infeasible partial solutions
-        if( location[boat] != location[chicken]):
+        # does valid partial solution break the constraints?
+        if( valid and location[boat] != location[chicken]):
             if( location[chicken]==location[fox]):
                 soln.reason = 'fox eats chicken'
                 soln.breaks_constraints= True
@@ -122,17 +125,19 @@ def Evaluate(soln: candidateSolution ):
                 soln.quality=-1
                 break
         #check for goal
-        if (location == [1,1,1,1]):
+        if (valid and location == [1,1,1,1]):
             soln.quality=1
+            soln.breaks_constraints= False
             print('goal reached')
             break
-    return reason
+    
+    
             
 #==================================================================            
-def to_display(soln:candidateSolution):
-    nummoves= len(soln.variableValues)
+def display(soln:CandidateSolution):
+    nummoves= len(soln.variable_values)
     movelist = ""
-    for move in soln.variableValues:
+    for move in soln.variable_values:
         #movelist = movelist + " -> " + moveNames [move] 
-        movelist = movelist + "->" + moveNames [move] 
-    return movelist
+        movelist = movelist + "->" + move_names [move] 
+    print( movelist)
