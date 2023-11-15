2030c2795edc5ebd90062b7f51a8ef69a409edff
Jim-smith <jim-smith@users.noreply.github.com>
2023-11-15 22:07:54 +0000

    working on workbooks 2 and 3

---
+++ Learning_Materials/week 2 /workbook2_utils.py
@@ -0,0 +1,225 @@
+import ipywidgets as widgets
+from IPython.display import clear_output
+
+
+# ======================================================
+class CandidateSolution:
+    def __init__(self):
+        self.variable_values = []
+        self.quality = 0
+        self.depth = 0
+
+
+# ======================================================
+# python 3 lets us define the types of parameters if we want to
+def is_atgoal(soln: CandidateSolution):
+    if soln.quality == 1:
+        return True
+    else:
+        return False
+
+
+# ======================================================
+
+# define the encoding we will use for moves
+move_names = [
+    "empty_0to1",
+    "Grain_0to1",
+    "Chicken_0to1",
+    "Fox_0to1",
+    "empty_1to0",
+    "Grain_1to0",
+    "Chicken_1to0",
+    "Fox_1to0",
+]
+
+# moveNames = ["b01","G01","C01","F01","b10","G10", "C10", "F10"]
+
+
+# =====================================================
+def evaluate(soln: CandidateSolution) -> str:
+    location = [0, 0, 0, 0]
+    reason = ""
+    boat = 3
+    grain = 2
+    chicken = 1
+    fox = 0
+    for move in soln.variable_values:
+        if move == 0:
+            if location[boat] != 0:
+                reason = "boat is in wrong place"
+                soln.quality = -1
+                break
+            else:
+                location[boat] = 1
+        elif move == 1:
+            if location[boat] != 0 or location[grain] != 0:
+                reason = "boat and/or grain is in wrong place"
+                soln.quality = -1
+                break
+            else:
+                location[boat] = location[grain] = 1
+        elif move == 2:
+            if location[boat] != 0 or location[chicken != 0]:
+                reason = "boat and/or chicken is in wrong place"
+                soln.quality = -1
+                break
+            else:
+                location[boat] = location[chicken] = 1
+        elif move == 3:
+            if location[boat] != 0 or location[fox] != 0:
+                reason = " boat and/or fox is in wrong place"
+                soln.quality = -1
+                break
+            else:
+                location[boat] = location[fox] = 1
+        elif move == 4:
+            if location[boat] != 1:
+                reason = "boat is in wrong place"
+                soln.quality = -1
+                break
+            else:
+                location[boat] = 0
+        elif move == 5:
+            if location[boat] != 1 or location[grain] != 1:
+                reason = "boat and/or grain is in wrong place"
+                soln.quality = -1
+                break
+            else:
+                location[boat] = location[grain] = 0
+        elif move == 6:
+            if location[boat] != 1 or location[chicken != 1]:
+                reason = " boat and/or chicken is in wrong place"
+                soln.quality = -1
+                break
+            else:
+                location[boat] = location[chicken] = 0
+        elif move == 7:
+            if location[boat] != 1 or location[fox] != 1:
+                reason = " boat and/or fox is in wrong place"
+                soln.quality = -1
+                break
+            else:
+                location[boat] = location[fox] = 0
+
+        else:
+            print("error- unknown move encountered: " + str(move))
+
+        # check for infeasible partial solutions
+        if location[boat] != location[chicken]:
+            if location[chicken] == location[fox]:
+                reason = "fox eats chicken"
+                soln.quality = -1
+                break
+            if location[chicken] == location[grain]:
+                reason = "chicken eats grain"
+                soln.quality = -1
+                break
+        # check for goal
+        if location == [1, 1, 1, 1]:
+            soln.quality = 1
+            print("goal reached")
+            break
+    return reason
+
+
+# ==================================================================
+def translate_solution_as_string(soln: CandidateSolution):
+    len(soln.variable_values)
+    movelist = ""
+    for move in soln.variable_values:
+        # movelist = movelist + " -> " + moveNames [move]
+        movelist = movelist + "->" + move_names[move]
+    return movelist
+
+
+# ================================================================
+
+
+def create_multiple_choice_widget(description, options, correct_answer):
+    if correct_answer not in options:
+        options.append(correct_answer)
+
+    correct_answer_index = options.index(correct_answer)
+
+    radio_options = [(words, i) for i, words in enumerate(options)]
+    alternative = widgets.RadioButtons(
+        options=radio_options, description="", disabled=False
+    )
+
+    description_out = widgets.Output()
+    with description_out:
+        print(description)
+
+    feedback_out = widgets.Output()
+
+    def check_selection(b):
+        a = int(alternative.value)
+        if a == correct_answer_index:
+            s = "\x1b[6;30;42m" + "Correct." + "\x1b[0m" + "\n"  # green color
+        else:
+            s = "\x1b[5;30;41m" + "Wrong. " + "\x1b[0m" + "\n"  # red color
+        with feedback_out:
+            clear_output()
+            print(s)
+        return
+
+    check = widgets.Button(description="submit")
+    check.on_click(check_selection)
+
+    return widgets.VBox([description_out, alternative, check, feedback_out])
+
+
+Q0 = create_multiple_choice_widget(
+    "What type of search is the algorithm below implementing?",
+    ["Constructive", "Perturbative"],
+    "Constructive",
+)
+Q1 = create_multiple_choice_widget(
+    "From your understanding of Depth-First Search, why did the algorithm fail to complete?",
+    ["It completed", "It got stuck in a loop", "It was not allowed enough iterations"],
+    "It got stuck in a loop",
+)
+Q2 = create_multiple_choice_widget(
+    "Is depth-first search complete", ["yes", "no"], "no"
+)
+text3 = "What is the minimum depth needed " "to solve the fox-chicken-grain problem?"
+Q3 = create_multiple_choice_widget(
+    text3,
+    ["4", "5", "6", "7", "8"],
+    "7",
+)
+text4 = (
+    "Will imposing a maximum depth on the solution  "
+    "make Depth-First Search complete successfully "
+    "for every problem"
+)
+Q4 = create_multiple_choice_widget(
+    text4,
+    ["yes", "no"],
+    "no",
+)
+
+text5 = (
+    "Does the depth of the solution "
+    "found by breadth-first match that needed "
+    "to solve using the amended version of depth-first?"
+)
+Q5 = create_multiple_choice_widget(
+    text5,
+    ["yes", "no"],
+    "yes",
+)
+Q6 = create_multiple_choice_widget(
+    "Will the code below finish in success?", ["yes", "no"], "yes"
+)
+Q7 = create_multiple_choice_widget(
+    "Are there more examined solutions for breadth or depth?",
+    ["breadth", "depth"],
+    "breadth",
+)
+Q8 = create_multiple_choice_widget(
+    "At any point does breadth or depth have a bigger open list?",
+    ["breadth", "depth"],
+    "breadth",
+)
--- Learning_Materials/week 3 /workbook3_utils.py
+++ Learning_Materials/week 3 /workbook3_utils.py
@@ -1,14 +1,21 @@
-import matplotlib.pyplot as plt
 import copy
-import numpy as np
 from time import sleep
-from IPython.display import clear_output,display
+
+import ipywidgets as widgets
+import matplotlib.pyplot as plt
+import numpy as np
+from IPython.display import clear_output
 
 
+# ======================================================
+class CandidateSolution:
+    def __init__(self):
+        self.variable_values = []
+        self.quality = 0
 
 
-#==========================================================
-class Maze: # this assumes maze is rectangular
+# ==========================================================
+class Maze:  # this assumes maze is rectangular
     # TODO add bounds checking for valid ids
     def __init__(self):
         self.contents = []
@@ -17,247 +24,264 @@ class Maze: # this assumes maze is rectangular
         self.start = 0
         self.goal = 0
 
-    def setup(self, 
-                  mazefilename:str =None,
-                  start:tuple=(0,0),
-                  end:tuple= (10,10)):
-        self.loadFromTxt("maze.txt")
-        self.setStart(0,9)
-        self.setGoal(20,11)
+    def setup(
+        self, mazefilename: str = "", start: tuple = (0, 0), end: tuple = (10, 10)
+    ):
+        self.load_from_txt("maze.txt")
+        self.set_start(0, 9)
+        self.set_goal(20, 11)
 
-        self.showMaze()
+        self.show_maze()
 
-
-        #define the amount to add to the previous cellid for each move
+        # define the amount to add to the previous cellid for each move
         # can only do this once the maze has been read in so we know how big it is!
         self.left_move = -1
         self.right_move = 1
-        self.up_move = - (self.last_column_id)
-        self.down_move = (maze.last_column_id)
+        self.up_move = -(self.last_column_id)
+        self.down_move = self.last_column_id
         # define the set of move so we can iterate through them
-        self.moveSet = [leftMove,downMove,rightMove, upMove]
+        self.move_set = [self.left_move, self.down_move, self.right_move, self.up_move]
 
-        return moveSet
+        return self.move_set
 
-    def load_from_txt(self,filename):
-        file = open(filename, 'r')
+    def load_from_txt(self, filename):
+        file = open(filename)
         for line in file.readlines():
             row = []
             for c in line:
-                if(c.isspace() and (c!="\n")):
+                if c.isspace() and (c != "\n"):
                     row.append(1)
-                elif(c!="\n"):
+                elif c != "\n":
                     row.append(0)
             self.contents.append(row)
         self.height = len(self.contents)
         self.width = len(self.contents[0])
-        self.last_column_id = self.width -1
+        self.last_column_id = self.width - 1
 
-                        
-    def show_maze(self,cmap="Set1"):
+    def show_maze(self, cmap="Set1"):
         green = 0.3
         yellow = 0.65
-    
-        #colour start and end point
-        self.colourCellFromId( self.start,green)
-        self.colourCellFromId( self.goal,yellow)
-        
-        plt.figure(figsize=(5,5))
-        plt.imshow(self.contents,cmap=cmap,norm=None)
-        plt.xticks(np.arange(0,self.width,2))
-        plt.yticks(np.arange(0,self.height,2))
 
-        
-    def setStart(self,x,y):
-        self.start = y + self.lastColumnId*x
-        
-    def setGoal(self,x,y):
-        self.goal = y + self.lastColumnId*x    
+        # colour start and end point
+        self.colour_cell_from_id(self.start, green)
+        self.colour_cell_from_id(self.goal, yellow)
 
-    def cellidToCoords(self,cellid):
-        y = cellid% (self.width -1)
-        x = int(cellid/(self.lastColumnId))
-        return x,y
-    
-    def coordsToCellid(self,x,y):
-        cellid = y+x*(self.lastColumnId)
+        plt.figure(figsize=(5, 5))
+        plt.imshow(self.contents, cmap=cmap, norm=None)
+        plt.xticks(np.arange(0, self.width, 2))
+        plt.yticks(np.arange(0, self.height, 2))
+
+    def set_start(self, x, y):
+        self.start = y + self.last_column_id * x
+
+    def set_goal(self, x, y):
+        self.goal = y + self.last_column_id * x
+
+    def cellid_to_coords(self, cellid):
+        y = cellid % (self.width - 1)
+        x = int(cellid / (self.last_column_id))
+        return x, y
+
+    def coords_to_cellid(self, x, y):
+        cellid = y + x * (self.last_column_id)
         return cellid
-    
-    def colourCellFromId(self,cellid,colour):
-        x,y = self.cellidToCoords(cellid)
+
+    def colour_cell_from_id(self, cellid, colour):
+        x, y = self.cellid_to_coords(cellid)
         self.contents[x][y] = colour
 
-    def evaluate(self, solution:CandidateSolution):
+    def evaluate(self, solution: CandidateSolution):
         """
-        method to give feedback on the value of a candidate solution
+        Method to give feedback on the value of a candidate solution.
 
         Parameters
         ----------
-        solution: CandidateSolution
-            the current attmept being tested
+        solution : CandidateSolution
+            the current attempt being tested
 
         Returns
-        --------
+        -------
         int
             the quality with -1 for invalid
         string
-            the reason why a soltion is invalid
+            the reason why a solution is invalid
         """
         reason = ""
-        quality =1
-        
-        #we only need to look at the last position for checking
-        position = solution.variableValues[-1]
-        
-        if(len(solution.variableValues)>1):
-            lastposition = solution.variableValues[-2]
-            xold,yold = self.cellidToCoords(lastposition)
-    
-        #check is in the maze
-        xnew,ynew = self.cellidToCoords(position)
-        if ((xnew < 0) or (xnew> self.lastColumnId) or (ynew < 0) or (ynew> (self.height -1))):
+        quality = 1
+
+        # we only need to look at the last position for checking
+        position = solution.variable_values[-1]
+
+        if len(solution.variable_values) > 1:
+            lastposition = solution.variable_values[-2]
+            xold, yold = self.cellid_to_coords(lastposition)
+
+        # check is in the maze
+        xnew, ynew = self.cellid_to_coords(position)
+        if (
+            (xnew < 0)
+            or (xnew > self.last_column_id)
+            or (ynew < 0)
+            or (ynew > (self.height - 1))
+        ):
             reason = "move takes route out of the maze"
             quality = -1
-            
+
         # and isn;t a wall- which are coded as zero
-        elif (maze.contents[xnew][ynew] == 0):
-            reason = "move from {},{} to {},{} takes route through wall".format(xold,yold,xnew,ynew)
+        elif self.contents[xnew][ynew] == 0:
+            reason = (
+                f"move from {xold},{yold} to {xnew},{ynew} takes route through wall"
+            )
             quality = -1
-    
+
         # and isn't going backwards
-        elif( len(solution.variableValues)>2 and position==solution.variableValues[-3]):
+        elif (
+            len(solution.variable_values) > 2
+            and position == solution.variable_values[-3]
+        ):
             reason = "move goes back on itself"
-            quality=-1
-            
-        else: # valid move
-            #get coords of goal
-            x2,y2 = maze.cellidToCoords(maze.goal)
-    
-            #calculate manhattan distance from pythagoras theorem
-            euclideanDistance = np.sqrt( (xnew - x2)*(xnew-x2) + (ynew-y2)*(ynew-y2))
-            manhattanDistance = np.abs(xnew-x2) + np.abs(ynew-y2)
-        
-            quality =  manhattanDistance
-            
-        
-        return quality,reason    
+            quality = -1
 
-#======================================================
+        else:  # valid move
+            # get coords of goal
+            x2, y2 = self.cellid_to_coords(self.goal)
+
+            # calculate manhattan distance from pythagoras theorem
+            np.sqrt((xnew - x2) * (xnew - x2) + (ynew - y2) * (ynew - y2))
+            manhattan_distance = np.abs(xnew - x2) + np.abs(ynew - y2)
+
+            quality = manhattan_distance
+
+        return quality, reason
+
+    def is_at_goal(self, soln: CandidateSolution) -> bool:
+        """Says is a soltino is at the maze goal."""
+        last_cell = soln.variable_values[len(soln.variable_values) - 1]
+        if last_cell == self.goal:
+            return True
+        else:
+            return False
 
 
-#======================================================
-class CandidateSolution:
-    def __init__(self):
-        self.variableValues = []
-        self.quality = 0
+# ======================================================
 
-#======================================================        
- 
-#python 3 lets us define the types of parameters if we want to
-def IsAtGoal(soln:CandidateSolution, theMaze:Maze): 
-    lastCell = soln.variableValues [ len(soln.variableValues) -1]
-    if (lastCell== theMaze.goal) :
-        return True
-    else:
-        return False
-    
-    
 
-   
-    
-    
-    
-    
-#======================================================           
-def displaySearchState(theMaze:Maze, current:CandidateSolution, openList,algname,steps, refresh_rate= 0.0075):
+# ======================================================
+def display_search_state(
+    the_maze: Maze,
+    current: CandidateSolution,
+    open_list,
+    algname,
+    steps,
+    refresh_rate=0.0075,
+):
     # make a copy of the maze so we can colour in the paths
-    newmaze = copy.deepcopy(theMaze)
- 
-    #set up the colour scheme
-    cmap = "Set1"
+    newmaze = copy.deepcopy(the_maze)
+
+    # set up the colour scheme
     green = 0.3
     yellow = 0.65
     blue = 0.2
     orange = 0.5
-    
-    startx,starty = newmaze.cellidToCoords(newmaze.start)
-    endx,endy = newmaze.cellidToCoords(newmaze.goal)
-    
-    #colour start and end point
-    newmaze.colourCellFromId( newmaze.start,green)
-    newmaze.colourCellFromId( newmaze.goal,yellow)
-    
-    # put the path on the current solution in orange
-    for position in current.variableValues:
-        newmaze.colourCellFromId(position,orange)
- 
-    
-    # put the endpoints of each partial solution in the openlist in blue
-    for item in openList:
-        lastpos = item.variableValues[-1]
-        newmaze.colourCellFromId(lastpos,blue)
 
-    #leave the old picture on screen for long enough to see then refresh
+    startx, starty = newmaze.cellid_to_coords(newmaze.start)
+    endx, endy = newmaze.cellid_to_coords(newmaze.goal)
+
+    # colour start and end point
+    newmaze.colour_cell_from_id(newmaze.start, green)
+    newmaze.colour_cell_from_id(newmaze.goal, yellow)
+
+    # put the path on the current solution in orange
+    for position in current.variable_values:
+        newmaze.colour_cell_from_id(position, orange)
+
+    # put the endpoints of each partial solution in the openlist in blue
+    for item in open_list:
+        lastpos = item.variable_values[-1]
+        newmaze.colour_cell_from_id(lastpos, blue)
+
+    # leave the old picture on screen for long enough to see then refresh
     sleep(refresh_rate)
     clear_output(wait=True)
-    plt.figure(figsize = (7.5,7.5))
+    plt.figure(figsize=(5, 5))
     title = f"progress for {algname} after testing {steps} solutions."
     title = title + "\n Current working candidate in orange.\n"
     title = title + "Blue cells indicate solutions on openList"
     plt.title(title)
-    plt.axis('off')
-    plt.imshow(newmaze.contents,cmap="Set1")
-    plt.show()  
-    #display(fig)
-    
-    
-#================================================================
-import ipywidgets as widgets
-import sys
-from IPython.display import display
-from IPython.display import clear_output
-from IPython.display import HTML
+    plt.axis("off")
+    plt.imshow(newmaze.contents, cmap="Set1")
+    plt.show()
+    # display(fig)
 
-def create_multipleChoice_widget(description, options, correct_answer):
+
+# ================================================================
+
+
+def create_multiple_choice_widget(description, options, correct_answer):
     if correct_answer not in options:
         options.append(correct_answer)
-    
+
     correct_answer_index = options.index(correct_answer)
-    
+
     radio_options = [(words, i) for i, words in enumerate(options)]
-    alternativ = widgets.RadioButtons(
-        options = radio_options,
-        description = '',
-        disabled = False
+    alternative = widgets.RadioButtons(
+        options=radio_options, description="", disabled=False
     )
-    
+
     description_out = widgets.Output()
     with description_out:
         print(description)
-        
+
     feedback_out = widgets.Output()
 
     def check_selection(b):
-        a = int(alternativ.value)
-        if a==correct_answer_index:
-            s = '\x1b[6;30;42m' + "Correct." + '\x1b[0m' +"\n" #green color
+        a = int(alternative.value)
+        if a == correct_answer_index:
+            s = "\x1b[6;30;42m" + "Correct." + "\x1b[0m" + "\n"  # green color
         else:
-            s = '\x1b[5;30;41m' + "Wrong. " + '\x1b[0m' +"\n" #red color
+            s = "\x1b[5;30;41m" + "Wrong. " + "\x1b[0m" + "\n"  # red color
         with feedback_out:
             clear_output()
             print(s)
         return
-    
+
     check = widgets.Button(description="submit")
     check.on_click(check_selection)
-    
-    
-    return widgets.VBox([description_out, alternativ, check, feedback_out])
 
-Q1 = create_multipleChoice_widget('What type of search is implemented?',['Constructive','Perturbative'],'Constructive')
-Q2 = create_multipleChoice_widget('Which algorithm found a path to the goal state after examining the fewest solutions?',['Depth-First','Breadth-First','Best-First','Astar'],'Depth-First')
-Q3 = create_multipleChoice_widget('How did the quality of solutions found by depth and breadth first compare?',['depth-first was better','breadth-first was better','they were the same'],'breadth-first was better')
-Q4 = create_multipleChoice_widget('Of the algorithms that found the optimal solution, which examined fewest solutions?',['Depth-First','Breadth-First','Best-First','Astar'],'Astar')
-Q5 = create_multipleChoice_widget('Does depth-first successfully solve all instances of this problem?',['yes','no'],'no')
-Q6 = create_multipleChoice_widget('Does the rank-order of efficiency the complete algorithms depend on the problem instance?',['yes','no'],'yes')
-         
+    return widgets.VBox([description_out, alternative, check, feedback_out])
+
+
+Q1 = create_multiple_choice_widget(
+    "What type of search is implemented?",
+    ["Constructive", "Perturbative"],
+    "Constructive",
+)
+Q2 = create_multiple_choice_widget(
+    "Which algorithm found a path to the goal state after examining the fewest solutions?",
+    ["Depth-First", "Breadth-First", "Best-First", "Astar"],
+    "Depth-First",
+)
+Q3 = create_multiple_choice_widget(
+    "How did the quality of solutions found by depth and breadth first compare?",
+    ["depth-first was better", "breadth-first was better", "they were the same"],
+    "breadth-first was better",
+)
+Q4 = create_multiple_choice_widget(
+    "Of the algorithms that found the optimal solution, which examined fewest solutions?",
+    ["Depth-First", "Breadth-First", "Best-First", "Astar"],
+    "Astar",
+)
+Q5 = create_multiple_choice_widget(
+    "Does depth-first successfully solve all instances of this problem?",
+    ["yes", "no"],
+    "no",
+)
+text6 = (
+    "Does the rank-order of efficiency for "
+    "the complete algorithms depend on the problem instance?"
+)
+Q6 = create_multiple_choice_widget(
+    text6,
+    ["yes", "no"],
+    "yes",
+)
