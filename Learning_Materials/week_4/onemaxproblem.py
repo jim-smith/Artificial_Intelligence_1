7fc0b469893b8c2038092173cbbe8300d7673293
Jim-smith <jim-smith@users.noreply.github.com>
2024-02-23 12:49:34 +0000

    updated lecture, workbook  and onemax problem for week 4

--- Learning_Materials/week_4/onemaxproblem.py
+++ Learning_Materials/week_4/onemaxproblem.py
@@ -1,7 +1,7 @@
 """Python implementation of superclass for problems
 author:james.smith@uwe.ac.uk 2023.
 """
-
+import numpy as np
 from problem import Problem
 class OneMaxBinary(Problem):
     """OneMax problem with N binary decisions.
@@ -48,19 +48,24 @@ class OneMaxBinary(Problem):
 
 class OneMaxContinuous(Problem):
     """OneMax problem with N  decisions in [0,1].
-    quality is sum of the  decision variables
     uses assertions to ensure solutions are valid
+    and self.value_set is interpeted as limits (min,max) on acceptable range of values
     
-    self.value_set is interpeted as limits (min,max) on acceptable range of values
+    The quality is sum of the  squares of the distance of decision variables from 1.0
+    i.e sum of (1.0 - decision)^2
+    This means we can also provide a gradient function
+    
     """
 
     def __init__(self, N = 20):
         self.numdecisions: int = N
         self.value_set: list = [0,1]
+        self.gradient:np.array= np.zeros(N) 
+        self.target:np.array= np.ones(N)
 
     def evaluate(self, solution: list) -> tuple[int, str]:
         """Evaluate function.
-
+        Sum of squared distance from 1.0 for each decision
         Parameters
          ----------
          attempt : list
@@ -77,16 +82,26 @@ class OneMaxContinuous(Problem):
         """
         
         score = 0.0
+        #convert to numpy array for ease
+        solution = np.asarray(solution)
+
         # validity checking
-        for val in solution:
-            errmsg1 = f"invalid value {val} found in solution"
-            assert val >= self.value_set[0],errmsg1
-            assert val <= self.value_set[1],errmsg1
+        max = np.max(solution)
+        min = np.min(solution)
+        assert  min >= self.value_set[0],f'Error: found value {min} outside valid range'
+        assert max <= self.value_set[1],f'Error: found value {max} outside valid range'
             
-        errmsg = f"solution has length {len(solution)} should be {self.numdecisions}"
-        assert len(solution)== self.numdecisions, errmsg2
+        errmsg = f"solution has length {solution.shape[0]} should be {self.numdecisions}"
+        assert solution.shape[0]== self.numdecisions, errmsg2
         
-        # calculate score
-        for i in range (self.numdecisions):
-            score += solution[i]
+        # calculate gradient and score
+        self.gradient = self.target-solution
+        score = np.square(self.gradient).sum() 
+        # round to 6 sdignificant digits
+        score=round(score,2)
+            
+        # toggle flag on whether gradient wil lbe new
         return score, ""
+    
+    def get_gradient(self):
+        return self.gradient
