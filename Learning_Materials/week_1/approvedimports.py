8d3a1eea29fd8da60096a621641cd98f83ce1a25
Nate <nathan_duran@hotmail.com>
2025-01-21 18:15:04 +0000

    Minor fixes for completed/DEWIS
    Fixed from maze import maze -> Maze in approved imports.

--- Learning_Materials/week_1/approvedimports.py
+++ Learning_Materials/week_1/approvedimports.py
@@ -2,4 +2,8 @@ import numpy as np
 from candidatesolution import CandidateSolution
 from problem import Problem
 from combinationproblem import CombinationProblem
+<<<<<<< HEAD
 from maze import Maze
+=======
+from maze import Maze
+>>>>>>> b1a9fd1 (Minor fixes for completed/DEWIS)
