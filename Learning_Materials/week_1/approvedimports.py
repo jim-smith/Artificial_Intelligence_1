d83740f071c4f828aa56badf8dccf1d89fae74b4
Nate <nathan_duran@hotmail.com>
2025-01-21 21:10:21 +0000

    Fixed WB 3 Being old version

--- Learning_Materials/week_1/approvedimports.py
+++ Learning_Materials/week_1/approvedimports.py
@@ -2,12 +2,4 @@ import numpy as np
 from candidatesolution import CandidateSolution
 from problem import Problem
 from combinationproblem import CombinationProblem
-<<<<<<< HEAD
-<<<<<<< HEAD
 from maze import Maze
-=======
-from maze import Maze
->>>>>>> b1a9fd1 (Minor fixes for completed/DEWIS)
-=======
-from maze import Maze
->>>>>>> origin/Portfolio-Fixes
